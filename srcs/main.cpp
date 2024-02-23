9d60af0ea589d0d66a70f09b94916eb26d025f98
Celine Junker <cjunker@c4r2p4.42nice.fr>
2024-02-23 15:16:24 +0100

    save

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -22,6 +22,20 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }  
 
+
+void error_callback(int error, const char* description) {
+    std::cout << "error: " << error << std::endl;
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cout << "Please select a .obj file to run this program." << std::endl;
@@ -33,14 +47,16 @@ int main(int argc, char *argv[]) {
         std::cout << "The file needs to be a valid .obj file." << std::endl;
         return -1;
     }
+    glfwSetErrorCallback(error_callback);
 
     Figure myFigure(filePath.string());
-
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
    // glfwWindowHint(GLFW_SAMPLES, 256);
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -58,10 +74,59 @@ int main(int argc, char *argv[]) {
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
 
-    std::vector<float> vertices =  myFigure.getVertexNormalList();
+//    std::vector<float> vertices =  myFigure.getVertexNormalList();
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+
+    //object d opengl : VBO cree une zone memoire dans le gpu, on envoie tous les sommets enssemble
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  //genere l identifiant
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  // aattache ke type a l objet
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copie les sommet definit dans la memoire du tampon
+
+    // GL_STATIC_DRAW : les données ne seront pas modifiées (ou rarement) ;
+    // GL_DYNAMIC_DRAW : les données seront souvent modifiées ;
+    // GL_STREAM_DRAW : les données seront modifiées à chaque affichage.
+
+    unsigned int vertexShader = "
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+
+    void main(){
+        gl_Position=vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+    ";
+
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader;
+
 
     while(!glfwWindowShouldClose(window))
     {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
